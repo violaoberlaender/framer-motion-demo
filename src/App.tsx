807ba4978ddb4acac290965f85f7e790d57e007f
violaoberlaender <viola.oberlaender@gmail.com>
2026-07-15 18:59:28 +0200

    Create App component with header and tiles

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FadeInHeader from './components/FadeInHeader'
+import HoverTiles from './components/HoverTiles'
+import AnimatedList from './components/AnimatedList'
+
+export default function App() {
+  return (
+    <main className="app">
+      <FadeInHeader />
+      <HoverTiles />
+      <AnimatedList />
+    </main>
+  )
+}
